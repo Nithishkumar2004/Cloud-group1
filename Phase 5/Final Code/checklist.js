104ae48e4e13649bb526cc07bf195327550711f7
Nithishkumar2004 <118984538+Nithishkumar2004@users.noreply.github.com>
2023-10-31 01:25:10 +0530

    Final Submission

---
+++ Phase 5/Final Code/checklist.js
@@ -0,0 +1,28 @@
+function generateChecklist() {
+    const destination = document.getElementById('destination').value;
+    const departureDate = document.getElementById('departureDate').value;
+    const returnDate = document.getElementById('returnDate').value;
+    const tripPurpose = document.getElementById('tripPurpose').value;
+
+    // Set the checklist items based on user inputs
+    const checklistItems = {
+        clothing: `Clothing for your trip to ${destination}`,
+        accessories: `Accessories for your ${tripPurpose} trip`,
+        toiletries: `Toiletries for your travel from ${departureDate} to ${returnDate}`,
+        documents: `Travel Documents for your ${tripPurpose} trip`,
+    };
+
+
+    // Open a new window with the checklist
+    const checklistWindow = window.open('checklist1.html', '_blank');
+    checklistWindow.addEventListener('load', function () {
+        for (const key in checklistItems) {
+            if (checklistItems.hasOwnProperty(key)) {
+                const element = checklistWindow.document.getElementById(key);
+                element.textContent = checklistItems[key];
+            }
+        }
+    });
+
+
+}
